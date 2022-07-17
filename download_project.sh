a7ded4c4b6533f35c5c31ee3366950282f68b303
Observer46 <microplayerpl@gmail.com>
2022-07-17 20:19:35 +0200

    [#1] Adapt bucket paths

--- download_project.sh
+++ download_project.sh
@@ -10,10 +10,10 @@ do
     read -ra model_type_and_name <<< $model
     model_type=${model_type_and_name[0]}
     model_name=${model_type_and_name[1]}
-    mc cp minio/$model_type/$model_name.zip /workspace/$project_name/$model_type/$model_name.zip
+    mc cp minio/hydrological-simulations/models/$model_type/$model_name.zip /workspace/$project_name/$model_type/$model_name.zip
     unzip /workspace/$project_name/$model_type/$model_name.zip -d /workspace/$project_name/$model_type/$model_name
     rm /workspace/$project_name/$model_type/$model_name.zip
     if [ $model_type = "hydrus" ]; then
-        mc cp minio/hydrus-shapes/$1_$model_name.npy /workspace/$project_name/hydrus_shapes/$model_name.npy
+        mc cp minio/hydrological-simulations/projects/hydrus-shapes/$1_$model_name.npy /workspace/$project_name/hydrus_shapes/$model_name.npy
     fi
 done
