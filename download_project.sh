847cd53a98f01f0c1459791764870f0954406f68
Observer46 <microplayerpl@gmail.com>
2022-11-27 21:08:40 +0100

    hotfix - add recursive download flag

--- download_project.sh
+++ download_project.sh
@@ -3,4 +3,4 @@
 project_minio_path=$1
 
 mc alias set minio https://$ENDPOINT $ACCESS_KEY $SECRET_KEY --api S3v4
-mc cp $project_minio_path /workspace/
+mc cp $project_minio_path /workspace/ --recursive
